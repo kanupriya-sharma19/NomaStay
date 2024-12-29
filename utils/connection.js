22ad817ebaf8c58816f7d405f8d3b1144b141fee
kanupriya-sharma19 <sharmakanpuriya@gmail.com>
2024-12-29 19:28:20 +0530

    adding project files now

--- utils/connection.js
+++ utils/connection.js
@@ -1,12 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import mongoose from 'mongoose';
 
+
+
+import mongoose from 'mongoose';
 
 export const connectToDB = () => {
   mongoose
-    .connect(process.env.MONGODB_URL)
+    .connect(process.env.MONGODB_URL, {
+      
+      ssl: true  // Make sure SSL is enabled
+    })
     .then(() => console.log("Connected to DB"))
     .catch((err) => console.error("DB connection error:", err));
 };
